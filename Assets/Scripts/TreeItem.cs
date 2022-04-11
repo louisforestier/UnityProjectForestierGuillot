86c1a786454e9a1dca97c7e83cef02bb0ef6f94c
Louis Forestier <louis.forestier@etu.univ-poitiers.fr>
2022-04-11 15:41:08 +0200

    modif de treeview et treeitem
    commentatage de treeview
    changement des exception en indexoutofrangeexception
    ajout methode childcount pour treeitem et treeview

--- Assets/Scripts/TreeItem.cs
+++ Assets/Scripts/TreeItem.cs
@@ -10,47 +10,76 @@ public class TreeItem : MonoBehaviour
     public GameObject arrow;
     public Text textComponent;
 
+    /// <summary>
+    /// Property to get and set the text of the TreeItem.
+    /// </summary>
     public string Text
     {
         get => textComponent.text;
         set => textComponent.text = value;
     }
+
+    /// <summary>
+    /// Add a child to this TreeItem.
+    /// </summary>
+    /// <param name="treeItem"></param>
     public void AddChild(TreeItem treeItem)
     {
-        Debug.Log("Add child in tree item");
-
         treeItem.transform.SetParent(children.transform,false);
     }
 
+    /// <summary>
+    /// Remove the child at the index i. The GameObject is not destroyed.
+    /// </summary>
+    /// <param name="i"></param>
     public void RemoveChild(int i)
     {
         if (i < children.transform.childCount)
             children.transform.GetChild(i).parent = null;
-        else throw new Exception("This child doesn't exist.");
+        else throw new IndexOutOfRangeException("This child doesn't exist.");
     }
 
-
+    /// <summary>
+    /// Remove and destroy the child at index i.
+    /// </summary>
+    /// <param name="i"></param>
     public void RemoveAndDestroyChild(int i)
     {
         if (i < children.transform.childCount)
             Destroy(children.transform.GetChild(i));
-        else throw new Exception("This child doesn't exist.");
+        else throw new IndexOutOfRangeException("This child doesn't exist.");
     }
 
+    /// <summary>
+    /// Get the child at index i.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns></returns>
     public TreeItem GetChild(int i)
     {
         if (i < children.transform.childCount)
             return children.transform.GetChild(i).GetComponent<TreeItem>();
-        else throw new Exception("This child doesn't exist.");
+        else throw new IndexOutOfRangeException("This child doesn't exist.");
     }
 
+    /// <summary>
+    /// Show/Hide the children and rotate the arrow accordingly.
+    /// </summary>
     public void ChildrenVisible()
     {
         if (children.activeSelf)
             arrow.transform.rotation = new Quaternion(0, 0, 0.7f,0.7f);
         else arrow.transform.rotation = Quaternion.identity;
-        Debug.Log(arrow.transform.rotation);
         children.SetActive(!children.activeSelf);
+    }
+
+    /// <summary>
+    /// Return the number of children.
+    /// </summary>
+    /// <returns> number of children</returns>
+    public int ChildrenCount()
+    {
+        return children.transform.childCount;
     }
 
 }
--- Assets/Scripts/TreeView.cs
+++ Assets/Scripts/TreeView.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class TreeView : MonoBehaviour
 {
@@ -17,4 +19,48 @@ public class TreeView : MonoBehaviour
     {
         treeItem.transform.SetParent(root.transform,false);
     }
+
+    /// <summary>
+    /// Remove the child at the index i. The GameObject is not destroyed.
+    /// </summary>
+    /// <param name="i"></param>
+    public void RemoveChild(int i)
+    {
+        if (i < root.transform.childCount)
+            root.transform.GetChild(i).parent = null;
+        else throw new IndexOutOfRangeException("This child doesn't exist.");
+    }
+
+    /// <summary>
+    /// Remove and destroy the child at index i.
+    /// </summary>
+    /// <param name="i"></param>
+    public void RemoveAndDestroyChild(int i)
+    {
+        if (i < root.transform.childCount)
+            Destroy(root.transform.GetChild(i));
+        else throw new IndexOutOfRangeException("This child doesn't exist.");
+    }
+
+    /// <summary>
+    /// Get the child at index i.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns></returns>
+    public TreeItem GetChild(int i)
+    {
+        if (i < root.transform.childCount)
+            return root.transform.GetChild(i).GetComponent<TreeItem>();
+        else throw new IndexOutOfRangeException("This child doesn't exist.");
+    }
+
+    /// <summary>
+    /// Return the number of children.
+    /// </summary>
+    /// <returns> number of children</returns>
+    public int ChildrenCount()
+    {
+        return root.transform.childCount;
+    }
+
 }
